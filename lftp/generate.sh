e0a92c358f88c7fed835ce1f466ff17baf22c3c4
Sebastien Besson <seb.besson@gmail.com>
2018-06-07 15:41:10 +0100

    Add infrastructure for testing ftp

---
+++ lftp/generate.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+N=${N:-10000}
+THREADS=${THREADS:-10}
+USER=${USER:-anonymous}
+PASS=${PASS:-allowed@example.org}
+SERVER=${SERVER:-ftpserver}
+
+# Create data and checksum
+mkdir -p /tmp/test && cd /tmp/test
+for i in $(seq 1 $N); do
+  echo $date > $i
+done
+find * -type f -exec sha1sum {} \; |  grep -v SHASUMS > SHASUMS
+
+# Generate upload script
+echo "open -u $USER,$PASS $SERVER" > /tmp/upload.scp
+echo "mkdir /incoming/test" >> /tmp/upload.scp
+echo "set ftp:use-site-utime false" >> /tmp/upload.scp
+echo "set ftp:use-site-utime2 false" >> /tmp/upload.scp
+echo "mirror -p --parallel=$THREADS -R /tmp/test/ /incoming/test/" >> /tmp/upload.scp
+echo "bye" >> /tmp/upload.scp
